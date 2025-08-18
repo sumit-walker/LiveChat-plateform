777bfcae359ebc128cac3bf8787d0960d1fb08ad
sumit-walker <sumitraj72177@gmail.com>
2025-08-18 23:48:47 +0530

    fix error in login

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,16 +2,9 @@ import jwt from "jsonwebtoken";
 
 
 export const generateToken= (userId,res)=>{
-    // const token=jwt.sign({userId},process.env.JWT_SECRET,{
-    const jwtSecret = process.env.JWT_SECRET || (process.env.NODE_ENV === "development" ? "dev_secret_key_change_me" : null);
-    if (!jwtSecret) {
-        throw new Error("JWT_SECRET is not set. Configure it in environment variables.");
-    }
-
-    const token=jwt.sign({userId}, jwtSecret,{
+    const token=jwt.sign({userId},process.env.JWT_SECRET,{
         expiresIn:"7d",
     });
-    
 
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
